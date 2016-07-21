714eff7f8df13389d5e7cd685b2f023b2cb8e760
rsuriya <rsuriya0396@gmail.com>
2016-07-21 18:18:36 +0530

    SSS
    nnnn

---
+++ positive.java
@@ -0,0 +1,21 @@
+
+public class Ass1 {
+
+	
+	public static void main(String[] args) {
+		int a;
+		a=Integer.parseInt(args[0]);
+		if(a>0)
+		{
+			System.out.println("number is positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("number is negative");
+		}
+		else
+			{
+			System.out.println("number is neither nor positive");}
+			}
+
+}
