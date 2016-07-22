b7ac5936aa5d2bfd4a3454735d49dcece70a7cc4
rsuriya <rsuriya0396@gmail.com>
2016-07-22 15:42:11 +0530

    numberof
    oddddddd

---
+++ noofodddd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the number");
+	a=in.nextInt();
+	b=in.nextInt();
+	for(c=a;c<=b;c++)
+	{
+		if(c%2!=0)
+		{
+	System.out.println(c);
+		}
+		else{continue;}
+	}
+	
+	System.out.println(d);
+}
+
+
+}
