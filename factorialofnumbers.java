52cab11e98d3b4c3f742860486dd231baa5711e0
rsuriya <rsuriya0396@gmail.com>
2016-07-22 09:27:45 +0530

    factorial
    ff

---
+++ factorialofnumbers.java
@@ -0,0 +1,21 @@
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+	int a;
+	a=Integer.parseInt(args[0]);
+	int result=1,i=1;
+	if(a<=1)
+	{
+	System.out.println("the factorial is="+out);}
+		else
+		{
+		do
+		{result=result*i;
+		i++;
+		}
+		while(i<=x);
+		
+		System.out.println("the factorial is="+out);}
+			}
+			}
